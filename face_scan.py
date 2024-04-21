37f069ee106c66b8d543254fb7500a766ec6170f
Hezeria05 <hannahgracevsantos@iskolarngbayan.pup.edu.ph>
2024-04-21 17:14:31 +0800

    RAS-77 Established Validations in Input Field

--- face_scan.py
+++ face_scan.py
@@ -37,8 +37,10 @@ def start_camera(CameraFrame, scanbtn, LogVname, face_dataset, face_labels, name
             pred_name = name[int(pred)]
             cv2.putText(frame, pred_name, (x, y-30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            LogVname.configure(state='normal')  # Enable the entry for update
             LogVname.delete(0, tk.END)
             LogVname.insert(0, pred_name)
+            LogVname.configure(state='disabled')
 
         # Convert the image to PIL format and then to ImageTk format.
         cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
