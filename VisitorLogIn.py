bcf9f70efea6f5e0dc0a2a5ba33ebf594663e160
Hezeria05 <hannahgracevsantos@iskolarngbayan.pup.edu.ph>
2024-05-11 00:42:28 +0800

    Visitor login changes

--- VisitorLogIn.py
+++ VisitorLogIn.py
@@ -57,9 +57,13 @@ def on_login_click(homepage_window, sec_id, Home_indct, Visitor_indct, Resident_
     LbVname = CTkLabel(LogInEframe, text='Visitor Name', fg_color="transparent", font=("Inter", 15, "bold"), text_color="#333333")
     LbVname.place(relx=0.185, rely=0.2, anchor='n')
 
-    ResidID = CTkComboBox(LogInEframe, width=360.0, height=45, values=[resident[1] for resident in fetch_residents()], button_color="#DEE6EA",
-                          button_hover_color="#ADCBCF", corner_radius=8, border_width=1, border_color='#DEE6EA',
-                          dropdown_hover_color="#ADCBCF", fg_color="#DEE6EA")
+    # Inside the on_login_click function
+    ResidID = CTkComboBox(LogInEframe, width=360.0, height=45,
+                        values=["--Select--"] + [resident[1] for resident in fetch_residents()],
+                        button_color="#DEE6EA", button_hover_color="#ADCBCF",
+                        corner_radius=8, border_width=1, border_color='#DEE6EA',
+                        dropdown_hover_color="#ADCBCF", fg_color="#DEE6EA", state='readonly')
+    ResidID.set("--Select--")  # Set the placeholder as the initially selected item
     ResidID.place(relx=0.5, rely=0.43, anchor='n')
     LRname = CTkLabel(LogInEframe, text='Resident Address', fg_color="transparent", font=("Inter", 15, "bold"), text_color="#333333")
     LRname.place(relx=0.225, rely=0.37, anchor='n')
